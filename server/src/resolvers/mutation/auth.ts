4c80f8faaa2958d106cc81864d1295c90a64ce19
AlbinNils98 <145000303+AlbinNils98@users.noreply.github.com>
2025-11-24 16:05:22 +0100

    Made changes to auth cookie config

--- server/src/resolvers/mutation/auth.ts
+++ server/src/resolvers/mutation/auth.ts
@@ -25,7 +25,7 @@ export const authMutationResolvers: MutationResolvers<GraphQLContext> = {
     res.cookie("access_token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 24 * 60 * 60 * 1000,
     });
 
