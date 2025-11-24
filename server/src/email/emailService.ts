a142dceec22026dc147fecaad2c75ca3ad4a0963
AlbinNils98 <145000303+AlbinNils98@users.noreply.github.com>
2025-11-24 19:10:47 +0100

    Trying out different email provider as gmail smtp not working with render

--- server/src/email/emailService.ts
+++ server/src/email/emailService.ts
@@ -2,18 +2,20 @@ import nodemailer from 'nodemailer';
 
 export default async function sendEmail(recipient: string, subject: string, message: string, html?: string): Promise<void> {
 
-  const { GMAILUSER, GMAILPASS } = process.env;
+  const { MAILERSEND_SMTP_USER, MAILERSEND_SMTP_PASS } = process.env;
 
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.mailersend.net',
+    port: 587,
+    secure: false,
     auth: {
-      user: GMAILUSER,
-      pass: GMAILPASS,
+      user: MAILERSEND_SMTP_USER,
+      pass: MAILERSEND_SMTP_PASS,
     }
   });
 
   await transporter.sendMail({
-    from: `"RestaurantDemo" <${GMAILUSER}>`,
+    from: `"RestaurantDemo" <no-reply@test-r83ql3pmr3mgzw1j.mlsender.net>`,
     to: recipient,
     subject: subject,
     text: message,
